67c5a7850861f894cde0ecdc891b60c6ce077fa9
mosszhaodphil <mosszhao@Mosss-MacBook-Pro-2.local>
2014-12-08 10:43:19 +0000

    variable rename

--- param_user.m
+++ param_user.m
@@ -11,10 +11,11 @@ clear
 f   = 32; % CBF
 arterial_blood_volume = 0.81; % arterial blood volume
 tau = 1; % Bolus arrival time
-t1  = 1.3; % T1 relaxation of tissue
+t1_t = 1.3; % T1 relaxation of tissue
+t1_a = 1.65; % T1 relaxation of arterial blood, in postprocessing section of (ETP)
 t   = 0 : 0.1 : 5; % time variable
 m_0a = 1; % equilibrium magnetization of arterial blood
-inversion_efficiency = 0.9; % inversion efficiency alpha
+inversion_efficiency = 0.91; % inversion efficiency alpha, in postprocessing section of (ETP)
 crush_efficiency = 0.35; % percentage of arterial blood signal removed
 
 dispersion_type = 1; % no dispersion
