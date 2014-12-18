da10e3413d72dc056394aa60c79909a49cac5a34
mosszhaodphil <mosszhaodphil@gmail.com>
2014-12-18 16:01:25 +0000

    added comments

--- simulate_QUASAR_ASL.m
+++ simulate_QUASAR_ASL.m
@@ -1,6 +1,5 @@
 % This program simulate QUASAR ASL data from a set of user parameters in param_user.m
-% The output is a text file which consists of QUASAR ASL data
-% File name should be specified by user or by default 'output.txt'
+% The output is a saved in the folder output_yyyymmdd_HHMMSS under the same directory
 
 % Program begins
 
@@ -9,34 +8,33 @@ load('param_basis.mat');
 load('param_user.mat');
 
 date_time_format = 'yyyymmdd_HHMMSS'; % date and time format
-date_time_now = clock; % get vector of current time
-date_time = datestr(date_time_now, date_time_format); % convert current time vector to string
+date_time_now    = clock; % get vector of current time
+date_time        = datestr(date_time_now, date_time_format); % convert current time vector to string
+dir_name         = strcat('output_', date_time); % Default directory name
 
-dir_name = strcat('output_', date_time); % Default directory name
-
-file_name_quasar = 'signal_quasar'; % file name to save QUASAR ASL signal
-file_name_crushed = 'signal_crushed'; % file name to save crushed ASL signal
+file_name_quasar     = 'signal_quasar'; % file name to save QUASAR ASL signal
+file_name_crushed    = 'signal_crushed'; % file name to save crushed ASL signal
 file_name_noncrushed = 'signal_noncrushed'; % file name to save noncrushed ASL signal
-file_type_txt = '.txt';
-file_type_nifty = '.nii.gz';
+file_type_txt        = '.txt';
+file_type_nifty      = '.nii.gz';
 
 % Simulate QUASAR ASL signal and save it to file
-quasar_asl_signal = zeros(length(t), 1); % construct a vector to store ASL signals at different sampling points specified by variable t
-quasar_asl_signal = calculate_QUASAR_ASL_signal(t); % calculate QUASAR ASL signal
-quasar_nifty_file_handle = make_nifty_file(quasar_asl_signal); % make nifty file from ASL signal
-quasar_asl_figure_handle = plot_quasar_signal(quasar_asl_signal, t);
+quasar_asl_signal        = zeros(length(t), 1); % construct a vector to store QUASAR ASL signals at different sampling points specified by variable t
+quasar_asl_signal        = calculate_QUASAR_ASL_signal(t); % calculate QUASAR ASL signal
+quasar_nifty_file_handle = make_nifty_file(quasar_asl_signal); % make nifty file from QUASAR ASL signal
+quasar_asl_figure_handle = plot_quasar_signal(quasar_asl_signal, t); % plot the signal over time
 
 % Simulate crushed ASL signal and save it to file
-crushed_asl_signal = zeros(length(t), 1);
-crushed_asl_signal = calculate_delta_M_crush(t);
-crushed_nifty_file_handle = make_nifty_file(crushed_asl_signal);
-crushed_asl_figure_handle = plot_crushed_signal(crushed_asl_signal, t);
+crushed_asl_signal        = zeros(length(t), 1); % construct a vector to store Crushed ASL signals at different sampling points specified by variable t
+crushed_asl_signal        = calculate_delta_M_crush(t); % calculate Crushed ASL signal
+crushed_nifty_file_handle = make_nifty_file(crushed_asl_signal); % make nifty file from Crushed ASL signal
+crushed_asl_figure_handle = plot_crushed_signal(crushed_asl_signal, t); % plot the signal over time
 
 % Simulate noncrushed ASL signal and save it to file
-noncrushed_asl_signal = zeros(length(t), 1);
-noncrushed_asl_signal = calculate_delta_M_noncrush(t);
-noncrushed_nifty_file_handle = make_nifty_file(noncrushed_asl_signal);
-noncrushed_asl_figure_handle = plot_noncrushed_signal(noncrushed_asl_signal, t);
+noncrushed_asl_signal        = zeros(length(t), 1); % construct a vector to store Noncrushed ASL signals at different sampling points specified by variable t
+noncrushed_asl_signal        = calculate_delta_M_noncrush(t); % calculate Noncrushed ASL signal
+noncrushed_nifty_file_handle = make_nifty_file(noncrushed_asl_signal); % make nifty file from Noncrushed ASL signal
+noncrushed_asl_figure_handle = plot_noncrushed_signal(noncrushed_asl_signal, t); % plot the signal over time
 
 % Plot YY curve of three signals
 yy_figure_handle = plot_signal([quasar_asl_signal crushed_asl_signal noncrushed_asl_signal], t);
