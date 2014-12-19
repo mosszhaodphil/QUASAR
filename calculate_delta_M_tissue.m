c1840fa845c40440638627f25cb986b1c05603da
mosszhaodphil <mosszhao@Mosss-MacBook-Pro-2.local>
2014-12-19 11:32:31 +0000

    algebraic approach to convolution
    used algebraic approach in eq [11] of Ostergaard High Resolution paper
    to calculate delta M signal

--- calculate_delta_M_tissue.m
+++ calculate_delta_M_tissue.m
@@ -3,12 +3,14 @@
 % MA Chappell (2012) doi: 10.1002/mrm.24260 (MACD)
 % ET Petersen (2006) doi: 10.1002/mrm.20784 (ETP)
 % RB Buxton (1998) doi: 10.1002/mrm.1910400308 (RBB)
+% L Ostergaard (1996) doi: 10.1002/mrm.1910360510 (LO)
 
 % This function calculates ASL signal deltaM of tissue using Buxton's model (RBB)
 % The same method is also used in equation [5] of (MACQ)
 % delta_M_tissue = 2 * alpha * M0a * f * (c(t) * r(t) * m(t))
 % c(t) = exp(-1 / T1a) * a(t)
 % a(t) depends on dispersion
+% We use algebraic approach to calculate convolution, eq [11] of (LO)
 
 function delta_M_tissue = calculate_delta_M_tissue(t)
 
@@ -19,11 +21,11 @@ function delta_M_tissue = calculate_delta_M_tissue(t)
 	input_function       = zeros(length(t), 1); % c(t) of (MACQ)
 	residue_buxton       = zeros(length(t), 1); % r(t) of (MACQ)
 	magnetization_buxton = zeros(length(t), 1); % m(t) of (MACQ)
-	Residue = zeros(length(t), 1);
+	residue_product      = zeros(length(t), 1); % r(t) * m(t) of (MACQ)
 
 	% calculate c(t)
 	input_function = calculate_delivery_tissue_Buxton(t);
-	input_function_matrix = convert_to_low_tri(input_function);
+	input_function_matrix = convert_to_low_tri(input_function); % create a lower triangular matrix as in eq[11] of (LO)
 
 	% calculate r(t)
 	residue_buxton = calculate_residue_r_Buxton(t);
@@ -31,18 +33,16 @@ function delta_M_tissue = calculate_delta_M_tissue(t)
 	% calculate m(t)
 	magnetization_buxton = calculate_relaxation_m(t);
 
-
-	for j = 1 : length(t)
-		% calculate ASL signal
-		delta_M_tissue(j) = 2 * inversion_efficiency * m_0a * f * conv(input_function(j), residue_buxton(j) * magnetization_buxton(j));
-	end
-
+	% calculate r(t) * m(t)
 	for j = 1 : length(t)
 		% R(t) = r(t) * m(t)
-		Residue(j) = residue_buxton(j) * magnetization_buxton(j);
+		residue_product(j) = residue_buxton(j) * magnetization_buxton(j);
 	end
 
-	delta_M_tissue = 2 * inversion_efficiency * m_0a * f * delta_ti * input_function_matrix * Residue;
+	% calculate tissue signal
+	% C(tj) as in eq [10] of (LO)
+	% signal = 2 * alpha * m0a * f * C(tj)
+	delta_M_tissue = 2 * inversion_efficiency * m_0a * f * delta_ti * input_function_matrix * residue_product;
 
 end
 
