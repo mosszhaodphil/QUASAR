86c87dff8e2c46c03ab598e70310578c7402a3df
mosszhaodphil <mosszhao@Mosss-MacBook-Pro-2.local>
2014-12-29 18:31:45 +0000

    change function arguments
    last argument is optional to event handles

--- plot_crushed_signal.m
+++ plot_crushed_signal.m
@@ -1,8 +1,16 @@
 % This function plot time series of Crushed ASL Sequence over sampling time
 
-function figure_handle = plot_crushed_signal(crushed_asl_signal, t)
+function figure_handle = plot_crushed_signal(crushed_asl_signal, t, varargin)
 
 	figure('visible','off');
+
+	% Condition where the plot is displayed on GUI
+	if(length(varargin) > 0)
+		current_handles = varargin{1};
+		axes(current_handles.axes3);
+
+	end
+
 	ts = timeseries(crushed_asl_signal, t);
 	plot(ts, 'Color', 'r'); %plot the curve in red
 	xlabel('Time(sec)');
--- plot_noncrushed_signal.m
+++ plot_noncrushed_signal.m
@@ -1,8 +1,16 @@
 % This function plot time series of Noncrushed ASL Sequence over sampling time
 
-function figure_handle = plot_noncrushed_signal(noncrushed_asl_signal, t)
+function figure_handle = plot_noncrushed_signal(noncrushed_asl_signal, t, varargin)
 
 	figure('visible','off');
+
+	% Condition where the plot is displayed on GUI
+	if(length(varargin) > 0)
+		current_handles = varargin{1};
+		axes(current_handles.axes4);
+
+	end
+
 	ts = timeseries(noncrushed_asl_signal, t);
 	plot(ts, 'Color', 'g'); % plot the curve in green
 	xlabel('Time(sec)');
