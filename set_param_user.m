2638f0009ea11faa65a7209882510d47fcfc4a6c
mosszhaodphil <mosszhao@Mosss-MacBook-Pro-2.local>
2014-12-30 13:39:36 +0000

    set user params from input text
    not ideal function. need to use structures

--- set_param_user.m
+++ set_param_user.m
@@ -3,9 +3,9 @@
 % MA Chappell (2012) doi: 10.1002/mrm.24260 (MACD)
 % ET Petersen (2006) doi: 10.1002/mrm.20784 (ETP)
 
-function [] = set_param_user()
+function [] = set_param_user(varargin)
 
-	% Variables
+	% Default variables
 	% User input variables
 	f                     = 32 / 6000; % CBF need to convert from ml/100ml/min to ml/g/s
 	arterial_blood_volume = 0.81 / 100; % arterial blood volume in percentage
@@ -20,10 +20,22 @@ function [] = set_param_user()
 
 	dispersion_type       = 1; % no dispersion
 
+	if(length(varargin) == 1)
+		current_handles = varargin{1};
+		f = str2double(get(current_handles.et_cbf, 'String')) / 6000;
+		arterial_blood_volume = str2double(get(current_handles.et_abv, 'String')) / 100;
+		tau_t = str2double(get(current_handles.et_tau_t, 'String'));
+		tau_m = str2double(get(current_handles.et_tau_m, 'String'));
+		t1_t = str2double(get(current_handles.et_t1_t, 'String'));
+		t1_a = str2double(get(current_handles.et_t1_a, 'String'));
+		t = eval(get(current_handles.et_ti, 'String'));
+
+	end
+
 
 	% save the these variables in a file
 	filename = 'param_user.mat';
-	save(filename);
+	save(filename, 'f', 'arterial_blood_volume', 'tau_t', 'tau_m', 't1_t', 't1_a', 't', 'm_0a', 'inversion_efficiency', 'dispersion_type', 'crush_efficiency');
 
 end
 
