087e8f1c00f244616d3cc911564f1ada9259591d
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-02-02 15:25:03 +0000

    Change t dimension of nifty file
    t dimension is effectively the total number of phases (slices) times
    total number if Tis.
    Since last phase (slice) is discarded in quail, so the t dimension is 6
    * 13 = 78

--- make_nifty_file.m
+++ make_nifty_file.m
@@ -14,22 +14,28 @@ function nifty_file_handle = make_nifty_file(asl_signal)
 	% load dummy nifty file
 	nifty_file_handle = load_nii('dummy.nii.gz');
 
+	% Compute effective t dimension of nifty file
+	effective_slices = param_mr_str.n_slices - 1; % last slice (phase) is discarded in quasil
+	% The total number of element on t dimension is number of TIs of each slice (phase)
+	% multiply by total number of effective slices(phases)
+	t_dimension_length = effective_slices * length(asl_signal);
+
 	% modify nifty file header
 	% set dimension of 4D nifty matrix
 	% x and y are dimension of k space
 	% z is the number of slices
 	% t is the total number of sampling points
-	nifty_file_handle.hdr.dime.dim(2 : 5) = [param_mr_str.m, param_mr_str.m, param_mr_str.n_slices, length(asl_signal)];
+	nifty_file_handle.hdr.dime.dim(2 : 5) = [param_mr_str.m, param_mr_str.m, param_mr_str.n_slices, t_dimension_length];
 
 	% set max and min display intensity in the same range of signal intensity
 	nifty_file_handle.hdr.dime.cal_max = max(asl_signal);
 	nifty_file_handle.hdr.dime.cal_min = min(asl_signal);
 
 	% create an empty matrix based on dimension in header
-	asl_signal_matrix = zeros(param_mr_str.m, param_mr_str.m, param_mr_str.n_slices, length(asl_signal));
+	asl_signal_matrix = zeros(param_mr_str.m, param_mr_str.m, param_mr_str.n_slices, t_dimension_length);
 
 	% we assign asl_signal to be the element at middle of first slice
-	asl_signal_matrix(param_mr_str.m / 2, param_mr_str.m / 2, 1, :) = asl_signal;
+	asl_signal_matrix(param_mr_str.m / 2, param_mr_str.m / 2, 1, 1 : length(asl_signal)) = asl_signal;
 
 	% assign signal matrix to nifty file
 	nifty_file_handle.img = asl_signal_matrix;
