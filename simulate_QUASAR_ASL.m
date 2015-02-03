ff28ad83af032d106bd857b50ecad97876fbb94e
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-02-03 11:41:24 +0000

    New function added to simulate QUASAR script
    1 Use new make_nifty_file function to produce signal nifty files
    
    2 Make raw QUASAR ASL 4D matrix by make_raw_QUASAR_matrix function

--- simulate_QUASAR_ASL.m
+++ simulate_QUASAR_ASL.m
@@ -24,38 +24,51 @@ file_name_blood      = 'signal_blood'; % file name to save Blood ASL signal
 file_name_crushed    = 'signal_crushed'; % file name to save crushed ASL signal
 file_name_noncrushed = 'signal_noncrushed'; % file name to save noncrushed ASL signal
 file_name_aif 		 = 'signal_aif'; % file name to save arterial input function (AIF) signal
+file_name_tc		 = 'signal_tc'; % file name to save raw ASl (Tag minus control tc) signal
 file_type_txt        = '.txt'; % text file extension
 file_type_nifty      = '.nii.gz'; % nifty file extension
+
+position = [32 32 1]; % position on 4D matrix to assgin time series signal
 
 % Simulate QUASAR (Tissue) ASL signal and save it to file
 quasar_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store QUASAR ASL signals at different sampling points specified by variable t
 quasar_asl_signal        = calculate_QUASAR_ASL_signal(param_user_str.t); % calculate QUASAR ASL signal
-quasar_nifty_file_handle = make_nifty_file(quasar_asl_signal); % make nifty file from QUASAR ASL signal
+quasar_asl_matrix		 = make_4D_matrix(quasar_asl_signal, position); % make 4D matrix to save QUASAR ASL signal
+quasar_nifty_file_handle = make_nifty_file(quasar_asl_matrix); % make nifty file from QUASAR ASL signal
 quasar_asl_figure_handle = plot_quasar_signal(quasar_asl_signal, param_user_str.t); % plot the signal over time
 
 % Simulate Blood ASL signal and save it to file
 blood_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Blood ASL signals at different sampling points specified by variable t
 blood_asl_signal        = calculate_delta_M_blood(param_user_str.t); % calculate Blood ASL signal
-blood_nifty_file_handle = make_nifty_file(blood_asl_signal); % make nifty file from Blood ASL signal
+blood_asl_matrix		= make_4D_matrix(blood_asl_signal, position); % make 4D matrix to save Blood ASL signal
+blood_nifty_file_handle = make_nifty_file(blood_asl_matrix); % make nifty file from Blood ASL signal
 blood_asl_figure_handle = plot_blood_signal(blood_asl_signal, param_user_str.t); % plot the signal over time
 
 % Simulate crushed ASL signal and save it to file
 crushed_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Crushed ASL signals at different sampling points specified by variable t
 crushed_asl_signal        = calculate_delta_M_crush(param_user_str.t); % calculate Crushed ASL signal
-crushed_nifty_file_handle = make_nifty_file(crushed_asl_signal); % make nifty file from Crushed ASL signal
+crushed_asl_matrix		  = make_4D_matrix(crushed_asl_signal, position); % make 4D matrix to save Crushed ASL signal
+crushed_nifty_file_handle = make_nifty_file(crushed_asl_matrix); % make nifty file from Crushed ASL signal
 crushed_asl_figure_handle = plot_crushed_signal(crushed_asl_signal, param_user_str.t); % plot the signal over time
 
 % Simulate noncrushed ASL signal and save it to file
 noncrushed_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Noncrushed ASL signals at different sampling points specified by variable t
 noncrushed_asl_signal        = calculate_delta_M_noncrush(param_user_str.t); % calculate Noncrushed ASL signal
-noncrushed_nifty_file_handle = make_nifty_file(noncrushed_asl_signal); % make nifty file from Noncrushed ASL signal
+noncrushed_asl_matrix		 = make_4D_matrix(noncrushed_asl_signal, position); % make 4D matrix to save Noncrushed ASL signal
+noncrushed_nifty_file_handle = make_nifty_file(noncrushed_asl_matrix); % make nifty file from Noncrushed ASL signal
 noncrushed_asl_figure_handle = plot_noncrushed_signal(noncrushed_asl_signal, param_user_str.t); % plot the signal over time
 
 % Simulate AIF ASL signal and save it to file
 aif_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store AIF signals at different sampling points specified by variable t
 aif_asl_signal        = calculate_delivery_tissue_Buxton(param_user_str.t); % calculate AIF signal
-aif_nifty_file_handle = make_nifty_file(aif_asl_signal); % make nifty file from AIF signal
+aif_asl_matrix		  = make_4D_matrix(aif_asl_signal, position); % make 4D matrix to save AIF ASL signal
+aif_nifty_file_handle = make_nifty_file(aif_asl_matrix); % make nifty file from AIF signal
 aif_asl_figure_handle = plot_aif_signal(aif_asl_signal, param_user_str.t); % plot the signal over time
+
+% Make a raw ASL matrix from crushed and noncrushed signals
+% This is equivelant to Label(Tag) minus Control (tc) of raw ASL signal
+tc_asl_matrix      = make_raw_QUASAR_matrix(crushed_asl_matrix, noncrushed_asl_matrix);
+tc_nifty_file_handle = make_nifty_file(tc_asl_matrix);  % Save raw ASL matrix in nifty file
 
 % Plot summary curve (4x4) of four signals
 summary_figure_handle = subplot_signal([quasar_asl_signal blood_asl_signal crushed_asl_signal noncrushed_asl_signal], param_user_str.t);
@@ -83,6 +96,9 @@ print(noncrushed_asl_figure_handle, '-dpng', file_name_noncrushed, '-r300'); % s
 dlmwrite(strcat(file_name_aif, file_type_txt), noncrushed_asl_signal); % save noncrushed ASL data to a text file
 save_nii(aif_nifty_file_handle, strcat(file_name_aif, file_type_nifty)); % save noncrushed ASL nifty file
 print(aif_asl_figure_handle, '-dpng', file_name_aif, '-r300'); % save noncrushed ASL signal time series figure
+
+% Save raw ASL tc file
+save_nii(tc_nifty_file_handle, strcat(file_name_tc, file_type_nifty));
 
 print(summary_figure_handle, '-dpng', 'summary_plot', '-r300'); % save ASL signal time series figure
 
