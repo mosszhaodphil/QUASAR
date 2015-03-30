75a2439318e87b6ae0000e2d1fcc8b74719de403
mosszhaodphil <mosszhaodphil@gmail.com>
2015-03-30 18:11:55 +0100

    make 3D or 4D Nifty file
    Edit function to make 3D or 4D Nifty file depending on the dimension of
    input matrix

--- make_nifty_file.m
+++ make_nifty_file.m
@@ -4,26 +4,42 @@
 % Thirdly, we copy the content of asl_signal to the signal matrix in dummy file
 % Finally, we package the file and return its file handle
 
-function nifty_file_handle = make_nifty_file(signal_4D_matrix)
+function nifty_file_handle = make_nifty_file(signal_matrix)
 
 	% load dummy nifty file
 	nifty_file_handle = load_nii('dummy.nii.gz');
 
-	% get the dimension of input matrix
-	[x, y, z, t] = size(signal_4D_matrix);
+	if(ndims(signal_matrix) == 3)
+		% get the dimension of input matrix
+		[x, y, z] = size(signal_matrix);
 
-	% modify nifty file header
-	% set dimension of 4D nifty matrix
-	% x and y are dimension of k space
-	% z is the number of slices
-	% t is the total number of sampling points
-	nifty_file_handle.hdr.dime.dim(2 : 5) = [x, y, z, t];
+		% modify nifty file header
+		% set dimension of 4D nifty matrix
+		% x and y are dimension of k space
+		% z is the number of slices
+		% t is the total number of sampling points
+		nifty_file_handle.hdr.dime.dim(2 : 4) = [x, y, z];
+
+	end
+
+	if(ndims(signal_matrix) == 4)
+		% get the dimension of input matrix
+		[x, y, z, t] = size(signal_matrix);
+
+		% modify nifty file header
+		% set dimension of 4D nifty matrix
+		% x and y are dimension of k space
+		% z is the number of slices
+		% t is the total number of sampling points
+		nifty_file_handle.hdr.dime.dim(2 : 5) = [x, y, z, t];
+
+	end
 
 	% set max and min display intensity in the same range of signal intensity
-	nifty_file_handle.hdr.dime.cal_max = max(signal_4D_matrix(:));
-	nifty_file_handle.hdr.dime.cal_min = min(signal_4D_matrix(:));
+	nifty_file_handle.hdr.dime.cal_max = max(signal_matrix(:));
+	nifty_file_handle.hdr.dime.cal_min = min(signal_matrix(:));
 
 	% assign signal matrix to nifty file
-	nifty_file_handle.img = signal_4D_matrix;
+	nifty_file_handle.img = signal_matrix;
 
 end
