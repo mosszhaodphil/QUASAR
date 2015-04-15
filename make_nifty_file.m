6eaef403efa92b0c965d491e2bd8dfef54a0a5f1
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-04-15 08:48:09 +0100

    modify make nifty file
    Now it is capable of making both 3D and 4D nifty files

--- make_nifty_file.m
+++ make_nifty_file.m
@@ -1,29 +1,46 @@
-% This function makes a nifty file from a 4D of QUASAR ASL signal matrix
-% First, a dummy nifty file is loaded
+% This function makes a nifty file a signal matrix (3D or 4D)
+% First, a dummy nifty file is loaded according to the dimension of the input matrix
 % Secondly, we edit the header of this dummy file
 % Thirdly, we copy the content of asl_signal to the signal matrix in dummy file
 % Finally, we package the file and return its file handle
 
-function nifty_file_handle = make_nifty_file(signal_4D_matrix)
+function nifty_file_handle = make_nifty_file(signal_matrix)
 
-	% load dummy nifty file
-	nifty_file_handle = load_nii('dummy.nii.gz');
+	% 3D matrix
+	if(ndims(signal_matrix) == 3)
+		% load dummy nifty file
+		nifty_file_handle = load_nii('dummy_3d.nii.gz');
+		% get the dimension of input matrix
+		[x, y, z] = size(signal_matrix);
 
-	% get the dimension of input matrix
-	[x, y, z, t] = size(signal_4D_matrix);
+		% modify nifty file header
+		% set dimension of 4D nifty matrix
+		% x and y are dimension of k space
+		% z is the number of slices
+		% t is the total number of sampling points
+		nifty_file_handle.hdr.dime.dim(2 : 4) = [x, y, z];
+	end
 
-	% modify nifty file header
-	% set dimension of 4D nifty matrix
-	% x and y are dimension of k space
-	% z is the number of slices
-	% t is the total number of sampling points
-	nifty_file_handle.hdr.dime.dim(2 : 5) = [x, y, z, t];
+	% 4D matrix
+	if(ndims(signal_matrix) == 4)
+		% load dummy nifty file
+		nifty_file_handle = load_nii('dummy_4d.nii.gz');
+		% get the dimension of input matrix
+		[x, y, z, t] = size(signal_matrix);
+
+		% modify nifty file header
+		% set dimension of 4D nifty matrix
+		% x and y are dimension of k space
+		% z is the number of slices
+		% t is the total number of sampling points
+		nifty_file_handle.hdr.dime.dim(2 : 5) = [x, y, z, t];
+	end
 
 	% set max and min display intensity in the same range of signal intensity
-	nifty_file_handle.hdr.dime.cal_max = max(signal_4D_matrix(:));
-	nifty_file_handle.hdr.dime.cal_min = min(signal_4D_matrix(:));
+	nifty_file_handle.hdr.dime.cal_max = max(signal_matrix(:));
+	nifty_file_handle.hdr.dime.cal_min = min(signal_matrix(:));
 
 	% assign signal matrix to nifty file
-	nifty_file_handle.img = signal_4D_matrix;
+	nifty_file_handle.img = signal_matrix;
 
 end
