d702e510a1ed940ec46a5c8d2708615c95d50300
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-04-15 10:12:47 +0100

    production version of estimate CBF PV

--- estimate_cbf_pv.sh
+++ estimate_cbf_pv.sh
@@ -2,14 +2,14 @@
 datafile=$1
 
 # Use basil to do model fitting
-basil -i $datafile -m mask -o output --infertau --infert1 --inferart --pgm pvgm --pwm pvwm -@ basil_pv_simulation_options.txt 
+basil -i $datafile -m mask -o output --pgm pvgm --pwm pvwm --fast -@ basil_pv_simulation_options.txt 
 
 # Rescale and mask GM CBF
 fslmaths pvgm -bin gm_mask
-fslmaths ./output/step4/mean_ftiss -mul 6000 -div 0.91 -mas gm_mask estimated_cbf_gm
+fslmaths ./output/step1/mean_ftiss -mul 6000 -div 0.91 -mas gm_mask estimated_cbf_gm
 
 # Rescale and mask WM CBF
 fslmaths pvwm -bin wm_mask
-fslmaths ./output/step4/mean_fwm -mul 6000 -div 0.91 -mas wm_mask estimated_cbf_wm
+fslmaths ./output/step1/mean_fwm -mul 6000 -div 0.91 -mas wm_mask estimated_cbf_wm
 
 echo 'Finish'
