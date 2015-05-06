5d3538b893164bf1dc4bfb29b351e605bc8057e5
mosszhaodphil <mosszhaodphil@gmail.com>
2015-05-06 17:46:14 +0100

    handles both vector and 4D matrix

--- add_white_noise.m
+++ add_white_noise.m
@@ -1,17 +1,19 @@
 % This function adds white noise to signal
 % Input parameters:
-% input_signal: noise free signal
+% input_signal: noise free signal (vector or 4D)
 % snr: signal to noise ratio
 % Output:
-% noisy_signal: noisy signal
+% noisy_signal: noisy signal (vector or 4D)
 
 function noisy_signal = add_white_noise(input_signal, snr)
 	
 	% Check dimension
+	% Vector
 	if(ndims(input_signal) == 1)
 		noisy_signal = awgn(input_signal, snr, 'measured');
 	end
 
+	% 4D matrix
 	if(ndims(input_signal) == 4)
 		[x, y, z, t] = size(input_signal);
 
