4c3c86ae44d99e980b5fb75067608877603e7693
mosszhaodphil <mosszhaodphil@gmail.com>
2015-05-06 18:06:44 +0100

    modified script to create PV and noisy signals
    Plot not working yet

--- simulate_QUASAR_ASL.m
+++ simulate_QUASAR_ASL.m
@@ -21,102 +21,159 @@ date_time_now    = clock; % get vector of current time
 date_time        = datestr(date_time_now, date_time_format); % convert current time vector to string
 dir_name         = strcat('output_', date_time); % Default directory name
 
-file_name_tissue     = 'signal_tissue'; % file name to save Tissue ASL signal
-file_name_blood      = 'signal_blood'; % file name to save Blood ASL signal
-file_name_crushed    = 'signal_crushed'; % file name to save crushed ASL signal
-file_name_noncrushed = 'signal_noncrushed'; % file name to save noncrushed ASL signal
-file_name_aif        = 'signal_aif'; % file name to save arterial input function (AIF) signal
-file_name_tc         = 'signal_tc'; % file name to save raw ASl (Tag minus control tc) signal
-file_name_tc_noise   = 'signal_tc_noise';
-file_type_txt        = '.txt'; % text file extension
-file_type_nifty      = '.nii.gz'; % nifty file extension
+file_name_tissue_gm       = 'tissue_gm'; % file name to save Tissue ASL signal
+file_name_tissue_wm       = 'tissue_wm'; % file name to save Tissue ASL signal
+file_name_tissue_pv       = 'tissue_pv';
+file_name_tissue_pv_noise = 'tissue_pv_noise';
+file_name_blood           = 'arterial_blood'; % file name to save Blood ASL signal
+file_name_blood_noise     = 'arterial_blood_noise';
+file_name_aif_gm          = 'aif_gm';
+file_name_aif_wm          = 'aif_wm';
+file_name_aif_pv          = 'aif_pv';
+file_name_aif_pv_noise    = 'aif_pv_noise';
+file_name_tc_gm           = 'tc_gm'; % file name to save raw ASl (Tag minus control tc) signal
+file_name_tc_wm           = 'tc_wm'; % file name to save raw ASl (Tag minus control tc) signal
+file_name_tc_pv           = 'tc_pv'; % file name to save raw ASl (Tag minus control tc) signal
+file_name_tc_pv_noise     = 'tc_pv_noise'; % file name to save raw ASl (Tag minus control tc) signal with noise
+file_type_txt             = '.txt'; % text file extension
+file_type_nifty           = '.nii.gz'; % nifty file extension
 
-position = [1 1 1]; % position on 4D matrix to assgin time series signal
+% Mask and partial volume files
+mask = param_user_str.mask;
+pvgm = param_user_str.pvgm;
+pvwm = param_user_str.pvwm;
 
-% Simulate Tisue ASL signal and save it to file
-tissue_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Tissue ASL signals at different sampling points specified by variable t
-tissue_asl_signal        = calculate_delta_M_tissue(param_user_str.t); % calculate Tissue ASL signal
-tissue_asl_matrix		 = make_4D_matrix(tissue_asl_signal, position); % make 4D matrix to save Tissue ASL signal
-tissue_nifty_file_handle = make_nifty_file(tissue_asl_matrix); % make nifty file from Tissue ASL signal
-tissue_asl_figure_handle = plot_tissue_signal(tissue_asl_signal, param_user_str.t); % plot the signal over time
 
+% Three compartment model: arterial blood, GM tissue, and WM tisue.
+
+% 1 Arterial blood compartment
 % Simulate Blood ASL signal and save it to file
 blood_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Blood ASL signals at different sampling points specified by variable t
 blood_asl_signal        = calculate_delta_M_blood(param_user_str.t); % calculate Blood ASL signal
-blood_asl_matrix		= make_4D_matrix(blood_asl_signal, position); % make 4D matrix to save Blood ASL signal
+blood_asl_matrix        = make_4D_matrix(blood_asl_signal, mask); % make 4D matrix to save Blood ASL signal
 blood_nifty_file_handle = make_nifty_file(blood_asl_matrix); % make nifty file from Blood ASL signal
 blood_asl_figure_handle = plot_blood_signal(blood_asl_signal, param_user_str.t); % plot the signal over time
 
-% Simulate crushed ASL signal and save it to file
-crushed_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Crushed ASL signals at different sampling points specified by variable t
-crushed_asl_signal        = calculate_delta_M_crush(param_user_str.t); % calculate Crushed ASL signal
-crushed_asl_matrix		  = make_4D_matrix(crushed_asl_signal, position); % make 4D matrix to save Crushed ASL signal
-crushed_nifty_file_handle = make_nifty_file(crushed_asl_matrix); % make nifty file from Crushed ASL signal
-crushed_asl_figure_handle = plot_crushed_signal(crushed_asl_signal, param_user_str.t); % plot the signal over time
 
-% Simulate noncrushed ASL signal and save it to file
-noncrushed_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Noncrushed ASL signals at different sampling points specified by variable t
-noncrushed_asl_signal        = calculate_delta_M_noncrush(param_user_str.t); % calculate Noncrushed ASL signal
-noncrushed_asl_matrix		 = make_4D_matrix(noncrushed_asl_signal, position); % make 4D matrix to save Noncrushed ASL signal
-noncrushed_nifty_file_handle = make_nifty_file(noncrushed_asl_matrix); % make nifty file from Noncrushed ASL signal
-noncrushed_asl_figure_handle = plot_noncrushed_signal(noncrushed_asl_signal, param_user_str.t); % plot the signal over time
+% 2 GM Tissue Compartment
+param_user_str.f     = param_user_str.f_gm;
+param_user_str.tau_t = param_user_str.tau_t_gm;
+param_user_str.t1_t  = param_user_str.t1_t_gm;
+save('param_user.mat', 'param_user_str');
 
-% Simulate AIF ASL signal and save it to file
-aif_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store AIF signals at different sampling points specified by variable t
-aif_asl_signal        = calculate_delivery_tissue_Buxton(param_user_str.t); % calculate AIF signal
-aif_asl_matrix		  = make_4D_matrix(aif_asl_signal, position); % make 4D matrix to save AIF ASL signal
-aif_nifty_file_handle = make_nifty_file(aif_asl_matrix); % make nifty file from AIF signal
-aif_asl_figure_handle = plot_aif_signal(aif_asl_signal, param_user_str.t); % plot the signal over time
+% Simulate GM Tisue ASL signal and save it to file
+tissue_gm_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Tissue ASL signals at different sampling points specified by variable t
+tissue_gm_asl_signal        = calculate_delta_M_tissue(param_user_str.t); % calculate Tissue ASL signal
+tissue_gm_asl_matrix        = make_4D_matrix(tissue_gm_asl_signal, mask); % make 4D matrix to save Tissue ASL signal
+tissue_gm_nifty_file_handle = make_nifty_file(tissue_gm_asl_matrix); % make nifty file from Tissue ASL signal
+tissue_gm_asl_figure_handle = plot_tissue_signal(tissue_gm_asl_signal, param_user_str.t); % plot the signal over time
+
+% Simulate GM AIF ASL signal and save it to file
+aif_gm_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store AIF signals at different sampling points specified by variable t
+aif_gm_asl_signal        = calculate_delivery_tissue_Buxton(param_user_str.t); % calculate AIF signal
+aif_gm_asl_matrix        = make_4D_matrix(aif_gm_asl_signal, mask); % make 4D matrix to save AIF ASL signal
+aif_gm_nifty_file_handle = make_nifty_file(aif_gm_asl_matrix); % make nifty file from AIF signal
+aif_gm_asl_figure_handle = plot_aif_signal(aif_gm_asl_signal, param_user_str.t); % plot the signal over time
 
 % Make a raw ASL matrix from crushed and noncrushed signals
 % This is equivelant to Label(Tag) minus Control (tc) of raw ASL signal
-tc_asl_matrix        = make_raw_QUASAR_matrix(tissue_asl_matrix, blood_asl_matrix);
-tc_nifty_file_handle = make_nifty_file(tc_asl_matrix);  % Save raw ASL matrix in nifty file
+tc_gm_asl_matrix        = make_raw_QUASAR_matrix(tissue_gm_asl_matrix, blood_asl_matrix);
+tc_gm_nifty_file_handle = make_nifty_file(tc_gm_asl_matrix);  % Save raw ASL matrix in nifty file
 
-% Add noise
-tissue_asl_noise_signal = add_white_noise(tissue_asl_signal, param_user_str.snr);
-tissue_asl_noise_matrix = make_4D_matrix(tissue_asl_noise_signal, position);
-blood_asl_noise_signal  = add_white_noise(blood_asl_signal, param_user_str.snr);
-blood_asl_noise_matrix  = make_4D_matrix(blood_asl_noise_signal, position);
-tc_asl_noise_matrix        = make_raw_QUASAR_matrix(tissue_asl_noise_matrix, blood_asl_noise_matrix);
-tc_noise_nifty_file_handle = make_nifty_file(tc_asl_noise_matrix);  % Save raw ASL matrix in nifty file
+
+% 3 WM Tissue Compartment
+param_user_str.f     = param_user_str.f_wm;
+param_user_str.tau_t = param_user_str.tau_t_wm;
+param_user_str.t1_t  = param_user_str.t1_t_wm;
+save('param_user.mat', 'param_user_str');
+
+% Simulate WM Tisue ASL signal and save it to file
+tissue_wm_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store Tissue ASL signals at different sampling points specified by variable t
+tissue_wm_asl_signal        = calculate_delta_M_tissue(param_user_str.t); % calculate Tissue ASL signal
+tissue_wm_asl_matrix        = make_4D_matrix(tissue_wm_asl_signal, mask); % make 4D matrix to save Tissue ASL signal
+tissue_wm_nifty_file_handle = make_nifty_file(tissue_wm_asl_matrix); % make nifty file from Tissue ASL signal
+tissue_wm_asl_figure_handle = plot_tissue_signal(tissue_wm_asl_signal, param_user_str.t); % plot the signal over time
+
+% Simulate WM AIF ASL signal and save it to file
+aif_wm_asl_signal        = zeros(length(param_user_str.t), 1); % construct a vector to store AIF signals at different sampling points specified by variable t
+aif_wm_asl_signal        = calculate_delivery_tissue_Buxton(param_user_str.t); % calculate AIF signal
+aif_wm_asl_matrix        = make_4D_matrix(aif_wm_asl_signal, mask); % make 4D matrix to save AIF ASL signal
+aif_wm_nifty_file_handle = make_nifty_file(aif_wm_asl_matrix); % make nifty file from AIF signal
+aif_wm_asl_figure_handle = plot_aif_signal(aif_wm_asl_signal, param_user_str.t); % plot the signal over time
+
+% Make a raw ASL matrix from crushed and noncrushed signals
+% This is equivelant to Label(Tag) minus Control (tc) of raw ASL signal
+tc_wm_asl_matrix        = make_raw_QUASAR_matrix(tissue_wm_asl_matrix, blood_asl_matrix);
+tc_wm_nifty_file_handle = make_nifty_file(tc_wm_asl_matrix);  % Save raw ASL matrix in nifty file
+
+
+% Add Partial volume maps to GM and WM tissue signal
+tissue_pv_asl_matrix = add_partial_volume(tissue_gm_asl_matrix, tissue_wm_asl_matrix, pvgm, pvwm);
+tissue_pv_nifty_file_handle = make_nifty_file(tissue_pv_asl_matrix);
+
+aif_pv_asl_matrix = add_partial_volume(aif_gm_asl_matrix, aif_wm_asl_matrix, pvgm, pvwm);
+aif_pv_nifty_file_handle = make_nifty_file(aif_pv_asl_matrix);
+
+tc_pv_asl_matrix = make_raw_QUASAR_matrix(tissue_pv_asl_matrix, blood_asl_matrix);
+tc_pv_nifty_file_handle = make_nifty_file(tc_pv_asl_matrix);  % Save raw ASL matrix in nifty file
+
+% Add some noise
+snr = param_user_str.snr;
+blood_asl_noise_matrix        = add_white_noise(blood_asl_matrix, snr);
+blood_noise_nifty_file_handle = make_nifty_file(blood_asl_noise_matrix);
+
+aif_pv_asl_noise_matrix        = add_white_noise(aif_pv_asl_matrix, snr);
+aif_pv_noise_nifty_file_handle = make_nifty_file(aif_pv_asl_noise_matrix);
+
+tc_pv_asl_noise_matrix        = add_white_noise(tc_pv_asl_matrix, snr);
+tc_pv_noise_nifty_file_handle = make_nifty_file(tc_pv_asl_noise_matrix);
+
+tissue_pv_asl_noise_matrix        = add_white_noise(tissue_pv_asl_matrix, snr);
+tissue_pv_noise_nifty_file_handle = make_nifty_file(tissue_pv_asl_noise_matrix);
 
 % Plot summary curve (4x4) of four signals
-summary_figure_handle = subplot_signal([tissue_asl_signal blood_asl_signal crushed_asl_signal noncrushed_asl_signal], param_user_str.t);
+%summary_figure_handle = subplot_signal([tissue_asl_signal blood_asl_signal crushed_asl_signal noncrushed_asl_signal], param_user_str.t);
 
-% save parameters of current simulation
+% save parameters of current simulation to parameters.txt file
 save_parameters();
 
 % Save simulated ASL data file in the new directory
 mkdir(dir_name);
 cd(dir_name);
 
-dlmwrite(strcat(file_name_tissue, file_type_txt), tissue_asl_signal); % save tissue ASL data to a text file
-save_nii(tissue_nifty_file_handle, strcat(file_name_tissue, file_type_nifty)); % save tissue ASL nifty file
-print(tissue_asl_figure_handle, '-dpng', file_name_tissue, '-r300'); % save tissue ASL signal time series figure
-
+% Arterial blood compartment
 dlmwrite(strcat(file_name_blood, file_type_txt), blood_asl_signal); % save blood ASL data to a text file
 save_nii(blood_nifty_file_handle, strcat(file_name_blood, file_type_nifty)); % save blood ASL nifty file
-print(blood_asl_figure_handle, '-dpng', file_name_blood, '-r300'); % save blood ASL signal time series figure
+save_nii(blood_noise_nifty_file_handle, strcat(file_name_blood_noise, file_type_nifty));
+%print(blood_asl_figure_handle, '-dpng', file_name_blood, '-r300'); % save blood ASL signal time series figure
 
-dlmwrite(strcat(file_name_crushed, file_type_txt), crushed_asl_signal); % save crushed ASL data to a text file
-save_nii(crushed_nifty_file_handle, strcat(file_name_crushed, file_type_nifty)); % save crushed ASL nifty file
-print(crushed_asl_figure_handle, '-dpng', file_name_crushed, '-r300'); % save crushed ASL signal time series figure
+% GM tissue compartment
+dlmwrite(strcat(file_name_tissue_gm, file_type_txt), tissue_gm_asl_signal); % save GM tissue ASL data to a text file
+save_nii(tissue_gm_nifty_file_handle, strcat(file_name_tissue_gm, file_type_nifty)); % save GM tissue ASL nifty file
+save_nii(aif_gm_nifty_file_handle, strcat(file_name_aif_gm, file_type_nifty)); % save GM AIF ASL nifty file
+save_nii(tc_gm_nifty_file_handle, strcat(file_name_tc_gm, file_type_nifty));
+%print(tissue_gm_asl_figure_handle, '-dpng', file_name_tissue_gm, '-r300'); % save tissue ASL signal time series figure
 
-dlmwrite(strcat(file_name_noncrushed, file_type_txt), noncrushed_asl_signal); % save noncrushed ASL data to a text file
-save_nii(noncrushed_nifty_file_handle, strcat(file_name_noncrushed, file_type_nifty)); % save noncrushed ASL nifty file
-print(noncrushed_asl_figure_handle, '-dpng', file_name_noncrushed, '-r300'); % save noncrushed ASL signal time series figure
+% WM tissue compartment
+dlmwrite(strcat(file_name_tissue_wm, file_type_txt), tissue_wm_asl_signal); % save WM tissue ASL data to a text file
+save_nii(tissue_wm_nifty_file_handle, strcat(file_name_tissue_wm, file_type_nifty)); % save WM tissue ASL nifty file
+save_nii(aif_wm_nifty_file_handle, strcat(file_name_aif_wm, file_type_nifty)); % save WM AIF ASL nifty file
+save_nii(tc_wm_nifty_file_handle, strcat(file_name_tc_wm, file_type_nifty));
+%print(tissue_gm_asl_figure_handle, '-dpng', file_name_tissue_gm, '-r300'); % save tissue ASL signal time series figure
 
-dlmwrite(strcat(file_name_aif, file_type_txt), aif_asl_signal); % save noncrushed ASL data to a text file
-save_nii(aif_nifty_file_handle, strcat(file_name_aif, file_type_nifty)); % save noncrushed ASL nifty file
-print(aif_asl_figure_handle, '-dpng', file_name_aif, '-r300'); % save noncrushed ASL signal time series figure
 
-% Save raw ASL tc file
-save_nii(tc_nifty_file_handle, strcat(file_name_tc, file_type_nifty));
+% PV ASL Signal
+save_nii(tissue_pv_nifty_file_handle, strcat(file_name_tissue_pv, file_type_nifty));
+save_nii(aif_pv_nifty_file_handle, strcat(file_name_aif_pv, file_type_nifty));
+save_nii(tc_pv_nifty_file_handle, strcat(file_name_tc_pv, file_type_nifty));
 
-save_nii(tc_noise_nifty_file_handle, strcat(file_name_tc_noise, file_type_nifty))
 
-print(summary_figure_handle, '-dpng', 'summary_plot', '-r300'); % save ASL signal time series figure
+% Noisy ASL signal
+save_nii(tissue_pv_noise_nifty_file_handle, strcat(file_name_tissue_pv_noise, file_type_nifty));
+save_nii(aif_pv_noise_nifty_file_handle, strcat(file_name_aif_pv_noise, file_type_nifty));
+save_nii(tc_pv_noise_nifty_file_handle, strcat(file_name_tc_pv_noise, file_type_nifty));
+
+%print(summary_figure_handle, '-dpng', 'summary_plot', '-r300'); % save ASL signal time series figure
 
 % Copy the default files to result directory
 copyfile('../mask.nii.gz', '.'); % Copy mask file
