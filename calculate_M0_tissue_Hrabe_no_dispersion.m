68db23e73eec6ce390db0021976c22d3e595f605
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-05-08 11:06:57 +0100

    new function to calculate delta M0 without dispersion
    We use Hrabe’s solution to calculate M0 when there is no dispersion

--- calculate_M0_tissue_Hrabe_no_dispersion.m
+++ calculate_M0_tissue_Hrabe_no_dispersion.m
@@ -1,39 +1,47 @@
+% This function calculates the tissue magnetization in Hrabe's solution
+% Ref: J Hrabe (2003) doi:10.1016/j.jmr.2003.11.002 (JH)
+% Input:
+% vector t
+% Output:
+% Tissue magnetization, D(t) of eq [7]
 
+function tissue_m = calculate_M0_tissue_Hrabe_no_dispersion(t)
 
-function tissue_m = new_function_Hrabe(t)
-
+	% Load User and MRI parameters
 	load('param_user.mat');
 	load('param_basis.mat');
 
 	tissue_m = zeros(length(t), 1);
 
-	k        = 0;
+	% Intermediate parameters in eq [7]
+	%k        = 0;
 	t1_a_eff = 0;
 	t1_t_eff = 0;
 	R        = 0;
+	F        = 0;
 
+	% Implementation of eq [7]
 	for j = 1 : length(t)
 
+		% Correct T1 in Look-locker readout
 		t1_a_eff = correct_t1a_look_locker(t(j));
 		t1_t_eff = correct_t1t_look_locker(t(j));
-		%k = 1 / t1_a_eff - (1 / t1_t_eff + param_user_str.f / param_mr_str.lamda);
+
+		% Calculate R and F
 		R = 1 / t1_t_eff - 1 / t1_a_eff;
 		F = 2 * param_user_str.inversion_efficiency * param_user_str.m_0a * param_user_str.f / param_mr_str.lamda * exp(- t(j) / t1_t_eff);
 
+		% Calculate tissue magnetization, D(t) of eq [7]
 		if(t(j) < param_user_str.tau_t)
 			tissue_m(j) = 0;
 		end
 
 		if(t(j) >= param_user_str.tau_t && t(j) < param_user_str.tau_t + param_mr_str.tau_b)
 			tissue_m(j) = F / R * (exp(R * t(j)) - exp(R * param_user_str.tau_t));
-			%q = exp(k * t(j)) * (exp(-k * param_user_str.tau_t) - exp(-k * t(j))) / (k * (t(j) - param_user_str.tau_t));
-			%tissue_m(j) = 2 * param_user_str.m_0a * param_user_str.f * (t(j) - param_user_str.tau_t) * param_user_str.inversion_efficiency * exp(-t(j) / t1_a_eff) * q;
 		end
 
 		if(t(j) >= param_user_str.tau_t + param_mr_str.tau_b)
 			tissue_m(j) = F / R * (exp(R * (param_user_str.tau_t + param_mr_str.tau_b)) - exp(R * param_user_str.tau_t));
-			%q = exp(k * t(j)) * (exp(-k * param_user_str.tau_t) - exp(-k * (param_user_str.tau_t + param_mr_str.tau_b))) / (k * param_mr_str.tau_b);
-			%tissue_m(j) = 2 * param_user_str.m_0a * param_user_str.f * param_mr_str.tau_b * param_user_str.inversion_efficiency * exp(-t(j) / t1_a_eff) * q;
 
 		end
 	end
